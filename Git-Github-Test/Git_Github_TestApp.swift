ca03c351b55398e8dc875bb353565372eab80e0e
vaibhav indure <indurevaibhav9@gmail.com>
2026-02-02 01:09:57 -0800

    Initial Commit

---
+++ Git-Github-Test/Git_Github_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_Github_TestApp.swift
+//  Git-Github-Test
+//
+//  Created by rentamac on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Git_Github_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
